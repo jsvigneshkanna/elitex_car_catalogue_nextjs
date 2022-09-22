b29fa3ba351c3fb7476bd30154a88ea198c083aa
vigneshkanna <vkrishitaran@gmail.com>
2022-09-22 21:07:24 +0530

    footer banner added

--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
 export { default as Cart } from "./Cart";
 export { default as Footer } from "./Footer";
-// export { default as FooterBanner } from "./FooterBanner";
+export { default as FooterBanner } from "./FooterBanner";
 export { default as HeroBanner } from "./HeroBanner";
 export { default as Layout } from "./Layout";
 export { default as Navbar } from "./Navbar";
